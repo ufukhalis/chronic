8a72cf65633a483c420c9f53d570891dba3cd94c
ufuk halis <ufukhalis@gmail.com>
2019-08-22 20:47:13 +0200

    Some improvements

--- src/main/java/io/github/ufukhalis/service/ChronicService.java
+++ src/main/java/io/github/ufukhalis/service/ChronicService.java
@@ -12,26 +12,27 @@ import java.time.Duration;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
 
 @Service
 public class ChronicService {
-    private Map<String, Disposable> FLUX_TASK_MAP = new ConcurrentHashMap<>();
+    private Map<String, Disposable> TASK_MAP = new ConcurrentHashMap<>();
 
     public Mono<Task> createTask(Task task) {
 
         return Mono.fromCallable(() -> createScheduler(task))
                 .map(fluxTask -> {
-                    this.FLUX_TASK_MAP.put(task.getName(), fluxTask);
+                    this.TASK_MAP.put(task.getName(), fluxTask);
                     return task;
                 });
     }
 
     public Mono<Void> deleteTask(String taskName) {
         return Mono.fromRunnable(() ->
-            Optional.ofNullable(this.FLUX_TASK_MAP.get(taskName))
+            Optional.ofNullable(this.TASK_MAP.get(taskName))
                     .ifPresent(disposable -> {
                         disposable.dispose();
-                        this.FLUX_TASK_MAP.remove(taskName);
+                        this.TASK_MAP.remove(taskName);
                     })
         );
     }
@@ -41,13 +42,20 @@ public class ChronicService {
                 .doOnEach(ignore -> {
                    int result = runCommand(task.getCommand());
                    System.out.println("Task name : " + task.getName() + " result : " + result);
-                })
-                .subscribeOn(Schedulers.single())
+                }).log()
+                .subscribeOn(Schedulers.newSingle(task.getName()))
                 .subscribe();
     }
 
     private int runCommand(String command) {
-        return Try.of(() -> new ProcessBuilder("cmd", "/c", command).inheritIO().start().waitFor())
+        return Try.of(() -> new ProcessBuilder(cmdFunc.apply(command)).inheritIO().start().waitFor())
                 .getOrElseThrow(e -> new RuntimeException(e));
     }
+
+    private Function<String, String[]> cmdFunc = command -> {
+        boolean isWindows = System.getProperty("os.name")
+                .toLowerCase().startsWith("windows");
+
+        return isWindows ? new String[] { "cmd", "/c", command} : new String[] { "sh", "-c", command };
+    };
 }
